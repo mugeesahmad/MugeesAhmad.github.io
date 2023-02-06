d53c9f36744ac91b58f14bd2b3a5b50975fb2897
Mugees Ahmad <shmugeesahmad@gmail.com>
2023-02-06 18:17:34 +0500

    Neated the code

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-const toggleKey = document.querySelector('.mobile-toggle');
-const navbar = document.querySelector('.side-nav');
-const openBtn = document.querySelector('#nav-btn-open');
-const closeBtn = document.querySelector('#nav-btn-close');
+const toggleKey = document.querySelector(".mobile-toggle");
+const navBar = document.querySelector(".side-nav");
+const openBtn = document.querySelector("#nav-btn-open");
+const closeBtn = document.querySelector("#nav-btn-close");
+const navBtn = document.querySelectorAll("[data-nav-btns]");
 
-const navBtn = document.querySelectorAll('[data-nav-btns]');
-
-toggleKey.addEventListener('click', ()=>{
-  if (navbar.getAttribute('data-is-open') === 'close'){
-    navbar.setAttribute('data-is-open', 'open');
-    openBtn.style.display = 'none';
-    closeBtn.style.display = 'block';
-    
+toggleKey.addEventListener("click", () => {
+  if (navBar.getAttribute("data-is-open") === "close") {
+    navBar.setAttribute("data-is-open", "open");
+    openBtn.style.display = "none";
+    closeBtn.style.display = "block";
+  } else if (navBar.getAttribute("data-is-open") === "open") {
+    navBar.setAttribute("data-is-open", "close");
+    openBtn.style.display = "block";
+    closeBtn.style.display = "none";
   }
-  else if (navbar.getAttribute('data-is-open') === 'open'){
-    navbar.setAttribute('data-is-open', 'close');
-    openBtn.style.display = 'block';
-    closeBtn.style.display = 'none';    
-  }
-})
+});
 
-navBtn.forEach((btn)=>{
+navBtn.forEach((btn) => {
   console.log(btn);
-  btn.addEventListener('click', ()=>{
-    navbar.setAttribute('data-is-open', 'close');
-    openBtn.style.display = 'block';
-    closeBtn.style.display = 'none'; 
-    console.log('button is clicked!');
-  })
-})
+  btn.addEventListener("click", () => {
+    navBar.setAttribute("data-is-open", "close");
+    openBtn.style.display = "block";
+    closeBtn.style.display = "none";
+  });
+});
